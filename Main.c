34bd8b961de5191c1f735b5f87b4947077256e55
TheCodeFox-tech <78907865+TheCodeFox-tech@users.noreply.github.com>
2021-11-30 19:29:26 +0100

    Revert Main.c

--- Main.c
+++ Main.c
@@ -1,26 +1,8 @@
 
 #include <stdio.h>
 
-#include <winuser.h>
-
 int main() {
-   //printf("Hello World!");
+   printf("Hello World!");
    
-   int msgboxID = MessageBox(NULL, (LPCWSTR)L"Resource not available\nDo you want to try again?", (LPCWSTR)L"Account Details", MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2);
-   int a = 0;
-   
-   switch (msgboxID)
-    {
-    case IDCANCEL:
-        a = 1;
-        break;
-    case IDTRYAGAIN:
-        a = 2;
-        break;
-    case IDCONTINUE:
-        a = 3;
-        break;
-    }
-
-   return msgboxID;
+   return 0;
 }
