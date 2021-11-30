0927add325d28c40ceacea4095d6ca8ee5ca5ca1
TheCodeFox-tech <78907865+TheCodeFox-tech@users.noreply.github.com>
2021-11-30 19:24:04 +0100

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,8 +1,26 @@
 
 #include <stdio.h>
 
-int main() {
-   printf("Hello World!");
+#include <winuser.h>
 
-   return 0;
+int main() {
+   //printf("Hello World!");
+   
+   int msgboxID = MessageBox(NULL, (LPCWSTR)L"Resource not available\nDo you want to try again?", (LPCWSTR)L"Account Details", MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2);
+   int a = 0;
+   
+   switch (msgboxID)
+    {
+    case IDCANCEL:
+        a = 1;
+        break;
+    case IDTRYAGAIN:
+        a = 2;
+        break;
+    case IDCONTINUE:
+        a = 3;
+        break;
+    }
+
+   return msgboxID;
 }
